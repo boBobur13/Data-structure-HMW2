d9ef8444394650468488348e899b92e5b0f1620c
Brain17 <b.daminov@newuu.uz>
2025-11-13 21:56:19 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,45 +1,49 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-   int A[100];
-   int sizePQ = 0;
-   int q;
-    cin>>q;
-   cin.ignore();
-   
-    for(int i=0;i<q;i++){
+    int A[100];
+    int sizePQ = 0;
+    int q;
+    cin >> q;
+    cin.ignore();
+
+    for(int i = 0; i < q; i++){
         string cmd;
         getline(cin, cmd);
+
         if(cmd.substr(0,6) == "insert"){
-            int x=stoi(cmd.substr(7));
-            A[sizePQ]=x;
+            int x = stoi(cmd.substr(7));
+            A[sizePQ] = x;
             sizePQ++;
         }
-        else if (cmd=="get max"){
-            if(sizePQ==0) cout<<"EMPTY"<<endl;
+        else if (cmd == "get max"){
+            if(sizePQ == 0) cout << "EMPTY" << endl;
             else{
-                int maxVal=A[0];
-                for(int j=1;j<sizePQ;j++)
-                   if(A[j]>maxVal) maxVal=A[j];
-                cout<<maxVal<<endl;
+             int maxVal = A[0];
+            for(int j = 1; j < sizePQ; j++)
+             if(A[j] >= maxVal) maxVal = A[j];
+                cout << maxVal << endl;
             }
         }
-        else if(cmd=="extract max"){
-            if(sizePQ==0) cout<<"EMPTY"<<endl;
+        else if(cmd == "extract max"){
+            if(sizePQ == 0) cout << "EMPTY" << endl;
             else{
-                int maxVal=A[0], pos=0;
-                for(int j=1;j<sizePQ;j++){
-                    if(A[j] > maxVal){
-                       maxVal=A[j];
-                       pos=j;
+                int maxVal = A[0], pos = 0;
+                for(int j = 1; j < sizePQ; j++){
+                    if(A[j] >= maxVal){
+                        maxVal = A[j];
+                        pos = j;
                     }
                 }
-                cout<<maxVal<<endl;
-                for(int j=pos;j<sizePQ-1;j++) A[j]=A[j+1];
+                cout << maxVal << endl;
+                for(int j = pos; j < sizePQ-1; j++) A[j] = A[j+1];
                 sizePQ--;
             }
         }
-        else if(cmd=="size") cout<<sizePQ<<endl;
+        else if(cmd == "size") cout << sizePQ << endl;
     }
+
     return 0;
 }
+
