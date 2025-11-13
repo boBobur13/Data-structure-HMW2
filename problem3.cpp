577e89e4e7c61cb15e23feddd6ac18f941329ec3
Brain17 <b.daminov@newuu.uz>
2025-11-13 21:36:07 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int A[100], sizePQ=0;
+   int A[100];
+   int sizePQ = 0;
    int q;
     cin>>q;
+   cin.ignore();
+   
     for(int i=0;i<q;i++){
         string cmd;
-        cin>>cmd;
-        if(cmd=="insert"){
-            int x;
-            cin>>x;
+        getline(cin, cmd);
+        if(cmd.substr(0,6) == "insert"){
+            int x=stoi(cmd.substr(7));
             A[sizePQ]=x;
             sizePQ++;
         }
-        else if (cmd=="get"){
+        else if (cmd=="get max"){
             if(sizePQ==0) cout<<"EMPTY"<<endl;
             else{
                 int maxVal=A[0];
-                for(int j=1;j<sizePQ;j++) if(A[j]>maxVal) maxVal=A[j];
+                for(int j=1;j<sizePQ;j++)
+                   if(A[j]>maxVal) maxVal=A[j];
                 cout<<maxVal<<endl;
             }
         }
-        else if(cmd=="extract"){
+        else if(cmd=="extract max"){
             if(sizePQ==0) cout<<"EMPTY"<<endl;
             else{
                 int maxVal=A[0], pos=0;
                 for(int j=1;j<sizePQ;j++){
-                    if(A[j]>=maxVal){maxVal=A[j]; pos=j;}
+                    if(A[j] > maxVal){
+                       maxVal=A[j];
+                       pos=j;
+                    }
                 }
                 cout<<maxVal<<endl;
                 for(int j=pos;j<sizePQ-1;j++) A[j]=A[j+1];
