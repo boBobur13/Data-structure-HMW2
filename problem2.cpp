83dce9bd29398148c643ac2a85e546d7d29e862f
Brain17 <b.daminov@newuu.uz>
2025-11-13 21:48:02 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    int A[100];
-    for(int i=0;i<n;i++) cin >> A[i];
-  ////////////////////////////////////////////
-   
-  for(int i=n/2-1;i>=0;i--){
-        int idx=i;
-        while(true){
-            int l=2*idx+1;
-            int r=2*idx+2;
-            int smallest=idx;
-            if(l<n && A[l]<A[smallest]) smallest=l;
-            if(r<n && A[r]<A[smallest]) smallest=r;
-           
-            if(smallest!=idx){
-            int t= A[idx];
-            A[idx]= A[smallest];
-            A[smallest]=t;
-            idx=smallest;
-            }
-            else break;
-       }
-    }
-    /////////////////////////////////////////////
-    int heapSize=n;
-    int sorted[100];
-    for(int i =0;i<n;i++){
-        sorted[i]=A[0];
-        A[0]= A[heapSize-1];
-        heapSize--;
-        int idx =0;
-        while (true){
-            int l =2*idx+1;
-            int r =2*idx+2;
-            int smallest=idx;
-            if(l<heapSize && A[l]<A[smallest]) smallest =l;
-            if(r<heapSize && A[r]<A[smallest]) smallest=r;
-            if(smallest!=idx){int t=A[idx];A[idx]=A[smallest];A[smallest]=t; idx=smallest;}
-            else break;
-        }
-    }
+void heapDown(int A[], int heapSize, int idx){
+ while(true){
+ int l = 2*idx + 1;
+     int r = 2*idx + 2;
+        int smallest = idx;
+        if(l < heapSize && A[l] < A[smallest]) smallest = l;
+        if(r < heapSize && A[r] < A[smallest]) smallest = r;
+////////////////////////////////////////
+        if(l < heapSize && r < heapSize && A[l] == A[r] && A[l] < A[smallest]) smallest = l;
 
-    for(int i= 0;i<n;i++) cout <<sorted[i]<<" ";
-    cout<<endl;
+        if(smallest != idx){
+            swap(A[idx], A[smallest]);
+             idx = smallest;
+        } else break;
+    }
+}
+int main(){
+ int n;
+ cin >> n;
+ int A[100];
+  for(int i = 0; i < n; i++) cin >> A[i];
+    for(int i = n/2 - 1; i >= 0; i--) heapDown(A, n, i);
+    int heapSize = n;
+    int sorted[100];
+    for(int i = 0; i < n; i++){
+        sorted[i] = A[0];
+        A[0] = A[heapSize - 1];
+        heapSize--;
+        heapDown(A, heapSize, 0);
+    }
+    for(int i = 0; i < n; i++) cout << sorted[i] << " ";
+    cout << endl;
     return 0;
 }
+
